2c7dedd268eb57b4f629507c4b3546c302e50b41
Rakyat-Jelata <rjelata012@gmail.com>
2026-07-11 14:29:47 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Buitenzorg Labs Initialized.");
+    
+    // Inisialisasi fungsi umum akan di sini
+    // (misal: scroll observer, theme toggle, dll)
+});
 
